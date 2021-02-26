3094e34a72da07b2daee6aa83c88a77a84e87ae4
Angus Lee <angus.lee809@gmail.com>
2021-02-26 23:53:31 +1100

    feat: Set tasks as Done (#3)

--- src/components/TodoCard.tsx
+++ src/components/TodoCard.tsx
@@ -1,12 +1,44 @@
 import React from 'react';
-import { Box, Title, Media } from 'rbx';
-import { Todo } from '../models/todos';
+import { Box, Button, Icon, Title, Media } from 'rbx';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheckSquare } from '@fortawesome/free-regular-svg-icons';
+import { useDispatch } from 'react-redux';
+
+import { setTodoStatus } from '../action-creators/todos';
+import { Todo, TodoStatus } from '../models/todos';
 
 interface TodoCardProps {
   todo: Todo;
 }
 
+interface DoneButtonProps {
+  onClickHandler: () => void;
+}
+
+function DoneButton(props: DoneButtonProps) {
+  const size = 'medium';
+
+  return (
+    <Button
+      size={size}
+      outlined={false}
+      color="white"
+      tooltip={'Mark as Done'}
+      onClick={props.onClickHandler}
+    >
+      <Icon size={size}>
+        <FontAwesomeIcon icon={faCheckSquare} size={'lg'} />
+      </Icon>
+    </Button>
+  );
+}
+
 function TodoCard(props: TodoCardProps) {
+  const dispatch = useDispatch();
+
+  const onDoneButtonClick = () => {
+    dispatch(setTodoStatus(props.todo.id, TodoStatus.DONE));
+  };
   return (
     <Box>
       <Media>
@@ -14,6 +46,12 @@ function TodoCard(props: TodoCardProps) {
           <Title subtitle size={4}>
             {props.todo.content}
           </Title>
+        </Media.Item>
+
+        <Media.Item align="right">
+          {props.todo.status !== TodoStatus.DONE && (
+            <DoneButton onClickHandler={() => onDoneButtonClick()} />
+          )}
         </Media.Item>
       </Media>
     </Box>
