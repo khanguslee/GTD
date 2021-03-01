2d507059d3c3409d6e28cfeaa218ac43b267a17c
khanguslee <khanguslee@gmail.com>
2021-03-01 15:22:58 +1100

    fix: Load in dotenv variables into CRA

--- src/App.tsx
+++ src/App.tsx
@@ -9,22 +9,23 @@ import AddTodo from './components/AddTodo';
 import TodoList from './components/TodoList';
 import './App.scss';
 
-const app = new Realm.App(process.env.REALM_APP_ID || '');
-const googleClientId = process.env.GOOGLE_CLIENT_ID;
+require('dotenv').config();
+
+const app = new Realm.App(process.env.REACT_APP_REALM_APP_ID || '');
+const client_id = process.env.REACT_APP_GOOGLE_CLIENT_ID;
 
 function App() {
   // Open google on tap when user goes onto the app
   useEffect(() => {
     // FIXME: google-one-tap types library is missing definitions.
     // @ts-ignore
-    googleOneTap(
-      { client_id: googleClientId },
-      async (response: { credential: string }) => {
-        const credentials = Realm.Credentials.google(response.credential);
-        const user = await app.logIn(credentials);
-        console.log(`Logged in with id: ${user.id}`);
-      }
-    );
+    googleOneTap({ client_id }, async (response: { credential: string }) => {
+      const credentials = Realm.Credentials.google(response.credential);
+      const user = await app.logIn(credentials);
+      console.log(
+        `Logged in with id: ${user.id} and name: ${user.profile.name}`
+      );
+    });
   });
 
   return (
