d8315db640c972a2b195609a081708a17eb9bb47
khanguslee <khanguslee@gmail.com>
2021-03-03 12:01:56 +1100

    feat: Add the rest of the redux saga routines into login saga

--- src/sagas/user.ts
+++ src/sagas/user.ts
@@ -1,4 +1,4 @@
-import { all, takeEvery } from 'redux-saga/effects';
+import { all, put, takeEvery } from 'redux-saga/effects';
 import * as Realm from 'realm-web';
 
 import { ILoginUserAction } from '../action-creators/actionTypes';
@@ -6,22 +6,36 @@ import { loginUser } from '../action-creators/auth';
 import { AuthenticationType } from '../models/user';
 
 function* userLoginSaga(action: ILoginUserAction) {
-  const app = new Realm.App(process.env.REACT_APP_REALM_APP_ID || '');
-  switch (action.payload.type) {
-    case AuthenticationType.GOOGLE: {
-      const credentials = Realm.Credentials.google(action.payload.credentials);
-      const user: Realm.User<
-        globalThis.Realm.DefaultFunctionsFactory,
-        any,
-        globalThis.Realm.DefaultUserProfileData
-      > = yield app.logIn(credentials);
-      console.log(
-        `Logged in with id: ${user.id} and name: ${user.profile.name}`
-      );
-      break;
+  try {
+    const app = new Realm.App(process.env.REACT_APP_REALM_APP_ID || '');
+    switch (action.payload.type) {
+      case AuthenticationType.GOOGLE: {
+        // Request credentials from Realm
+        yield put(loginUser.request());
+        const credentials = Realm.Credentials.google(
+          action.payload.credentials
+        );
+        const user: Realm.User<
+          globalThis.Realm.DefaultFunctionsFactory,
+          any,
+          globalThis.Realm.DefaultUserProfileData
+        > = yield app.logIn(credentials);
+
+        // Login to realm app using google credentials
+        console.log(
+          `Logged in with id: ${user.id} and name: ${user.profile.name}`
+        );
+        yield put(loginUser.success());
+        break;
+      }
+      default:
+        console.error(`Invalid authentication type - ${action.payload.type}`);
+        yield put(loginUser.failure());
     }
-    default:
-      console.error(`Invalid authentication type - ${action.payload.type}`);
+  } catch (error) {
+    yield put(loginUser.failure(error));
+  } finally {
+    yield put(loginUser.fulfill());
   }
 }
 
